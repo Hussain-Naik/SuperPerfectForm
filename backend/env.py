4f26c6365575160c123f0a97b56f80801ead60e1
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2025-05-06 09:57:09 +0100

    Add initial commit

---
+++ backend/env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ['SECRET_KEY'] = 'django-insecure-c(ozvyhh@w8e#@gfu0%y6&)2wa)fwphihl6799(8r3x!7i*z1v'
+os.environ['DATABASE_URL'] = 'will be used to connect to the database'
+os.environ['DEV'] = '1'
