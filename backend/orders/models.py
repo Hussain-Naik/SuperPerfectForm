657d1314bbe186fba61963bef44582593b40ba33
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2025-05-22 21:25:42 +0100

    Add product model migrations

--- backend/orders/models.py
+++ backend/orders/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from menu.models import Menu
+# from menu.models import Menu
 
 # Create your models here.
 
@@ -14,13 +14,13 @@ class Order(models.Model):
     paid = models.BooleanField(default=False)
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    items = models.ManyToManyField(Menu, through='OrderItem')
+    items = models.ManyToManyField(Product, through='OrderItem')
     
     def __str__(self):
         return self.reference
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    product = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
