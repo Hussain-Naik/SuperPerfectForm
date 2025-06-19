d62c6b91a7b19af789e9fd41e58a4813f6ddfaae
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2025-06-19 08:25:53 +0100

    Add SSE view

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_asse',
     'rest_framework',
     'menu',
     'orders',
@@ -94,12 +95,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+if 'DEV' in os.environ:
+    DATABASES = {
+         'default': {
+             'ENGINE': 'django.db.backends.sqlite3',
+             'NAME': BASE_DIR / 'db.sqlite3',
+         }
+     }
+else:
+    DATABASES = {
+         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+     }
 
 
 # Password validation
--- backend/orders/views.py
+++ backend/orders/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import redirect
+from django_asse import SseStreamView, JsonEvent
+import asyncio
 from rest_framework import generics
 from .models import Order
 from .serializers import OrderSerializer
@@ -7,5 +8,10 @@ class OrderCreateView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
+class OrderStream(SseStreamView):
+    async def stream(self, request):
+        while True:
+            yield JsonEvent(event='order_update', data={'message': 'New Order update'})
+            await asyncio.sleep(5)
 
 
